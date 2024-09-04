e2e91cbdc17cf7e7fd503aaf29eba0d12a5233b4
Amoly93 <amalbajber93@gmail.com>
2024-09-04 09:25:54 +0300

    finally edit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ const data = [{ id: 1, name: 'Solar Panel 1', img: '01.jpg' }, { id: 2, name: 'S
 const list = []
 function App() {
     return (
-        <div className="App bg-neutral-200 h-auto fixed  flex justify-center  p-10">
-            <section class="text-center   ">
-                <div class=" space-y-7  text-pretty  ">
-                    <p class="text-6xl font-light align-middle"> Why we should be greening our homes with plants </p>
-                    <p class="text-base align-middle"> Solar panel module series provides unparalleled value, aesthetics, and reliability for homeowners while offering layout flexibility, several mounting options, and widespread component compatibility for installers </p>
+        <div className=" bg-neutral-200 h-screen">
+            <div className="flex flex-col items-center gap-10 conainer max-w-6x1 mx-auto py-10  ">
+                <div className ="items-center gap-5">
+                    <p className="text-lg text-center max-w-5x1"> Why we should be greening our homes with plants </p>
+                    <p className="text-base text-center max-w-4x1"> Solar panel module series provides unparalleled value, aesthetics, and reliability for homeowners while offering layout flexibility, several mounting options, and widespread component compatibility for installers </p>
                 </div>
-                <div class="grid grid-cols-1 lg:grid-cols-3 px-10 py-10 ">
+                <div class="grid lg:grid-cols-3 px-10 gap-16 py-10">
                     {data.map(i => {
                         return (
-                            <div class=" my-10 grid place-content-center relative    ">
+                            <div className="relative flex justify-center">
                                
-                                    <img src={i.img} alt="Solar Panel 1" class="h-72 w-80" />
+                                    <img src={i.img} alt="Solar Panel 1" className="h-72 w-80" />
                                
-                                <div class=" bg-white  h-40 w-64  grid place-content-center shadow-md drop-shadow-lg space-y-4  absolute left-24 -bottom-28  ">
-                                    <h2 class="text-lg font-extrabold">{i.name}</h2>
+                                <div className=" absolute left-auto right-auto -bottom-28 bg-white  h-36 w-64  grid place-content-center shadow-md drop-shadow-lg space-y-4  ">
+                                    <h2 className="text-lg font-extrabold">{i.name}</h2>
                                     <button class="bg-orange-500 px-6 py-2 text-white text-sm hover:bg-neutral-800 rounded-lg "> VIEW DETAILS </button>
                                 </div>
                             </div>
@@ -26,12 +26,12 @@ function App() {
                     })}
                 </div>
                 <div >
-                    <div class="h-20" ></div>
-                    <div class=" " >
-                        <button class="border-2 border-neutral-950 px-14 py-4 text-slate-800 hover:text-white text-sm hover:bg-neutral-950  rounded-lg"> LEARN MORE </button>
-                    </div>
+                    
+                 
+                        <button class="border-2 border-neutral-950 px-14 py-4 text-slate-800 hover:text-white text-sm hover:bg-neutral-950  rounded-lg my-20"> LEARN MORE </button>
+                  
                 </div>
-            </section>
+            </div>
         </div>
     );
 }
