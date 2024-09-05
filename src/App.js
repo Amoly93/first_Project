ee0db023a0872ab70a72ec1a8b83416eebde6fe3
Amoly93 <amalbajber93@gmail.com>
2024-09-05 14:21:06 +0300

    add delete function

--- src/App.js
+++ src/App.js
@@ -6,27 +6,22 @@ import { useState } from 'react';
 
 
 function App() {
-   const [data,setTest] = useState([
+   const [image,setImage] = useState([
   { id: 1, name: "Solar Panel 1", img: "01.jpg" },
  { id: 2, name: "Solar Panel 2", img: "solar_panel_32.jpg" },
    { id: 3, name: "Solar Panel 3", img: "solar_panel_3.jpg" },
  ])
+ //const [isVisible, setIsVisible] = useState(true);
 //.sort()
  //data.sort(function(a, b){return b - a});
 //data.reverse()
- function deleteData(item){
-    const index = data.indexOf(item);
+ function deleteData(item,index){
+   const newImage = image.filter((image) =>image !== item)
+   setImage(newImage)
 
-  
-  if (index >= 0) { 
-    data.splice(index, 1); 
-    data.filter((data) =>data !== item)
-  }
-    console.log(data); 
-   const newData = data.filter((data) =>data !== item)
+ }
 
-   setTest(newData)
-}
+
   return (
     <div className="bg-neutral-200 h-screen flex flex-col justify-center">
       <div className="flex flex-col items-center gap-16 conainer max-w-6xl mx-auto">
@@ -41,17 +36,16 @@ function App() {
             installers
           </p>
         </div>
-        <div className="grid lg:grid-cols-3 content-center gap-8">
+        <div className="flex justify-center gap-8">
       
-          {data.map((i) => {
+          {image.map((i,index) => {
             
             return (
-              <div className="relative flex justify-center h-fit">
+              <div className="relative flex justify-center  h-fit">
                 <img src={i.img} alt="Solar Panel 1" className="h-72 w-80" />
-                <span>
-                    
-               <button className="border-2 border-amber-600" onClick={() => deleteData(i)}  >x</button>
-                     </span>
+
+                {image.length!== 1 ? (<button  className=" absolute right-2 top-2 border-2 border-rose-500 rounded-full h-7 w-7 text-rose-600 font-bold  " onClick={() => deleteData(i,index)} >x</button>): null}
+              
                 <div className="absolute left-auto right-auto -bottom-28 bg-white h-36 w-64  grid place-content-center shadow-md drop-shadow-lg space-y-4">
                   <h2 className="text-lg font-extrabold">{i.name}</h2>
                   <button className="bg-orange-500 px-6 py-2 text-white text-sm hover:bg-neutral-800 rounded-lg">
