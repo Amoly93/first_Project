72c4eb8f41b3e340f56c8b2c2202a7bddf96c37b
Amoly93 <amalbajber93@gmail.com>
2024-09-09 14:42:03 +0300

    finally022

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
   const [textPrint, setTextPrint] = useState({text:'',ison: false})
 
   
-  function showPrint(T) {
+ const  showPrint = (T) =>{
     setTextPrint({text:T,ison:true});
   }
 
