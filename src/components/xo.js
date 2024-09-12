07c7d6f4f4ee7ce45135ddfff5d0d39cd545c876
Amoly93 <amalbajber93@gmail.com>
2024-09-12 10:45:08 +0300

    ox game

--- src/components/xo.js
+++ src/components/xo.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function XO() {
-  const [xo, setItem] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [item, setItem] = useState(["", "", "", "", "", "", "", "", ""]);
 
   const indexOfArrayOX = [
     [0, 1, 2],
@@ -17,52 +17,67 @@ function XO() {
   const [player, setPlayer] = useState("x");
 
   const xoFunc = (index) => {
-    const newArrayxo = [...xo];
+    const newArrayxo = [...item];
     newArrayxo[index] = player;
     setItem(newArrayxo);
-    if (player === "x"){
-      setPlayer( "o");
-    }else{
-      setPlayer( "x");
-    }
-  
+    setPlayer( player === "x"? "o":"x")
   };
 
-  const [win,setwin] = useState("") 
+ let win;
   const checkWin = () => {
 
     for (let i = 0; i < indexOfArrayOX.length; i++) {
       if (
         indexOfArrayOX[i].every((index) => {
-          return xo[index] === player;
+          console.log(i)
+          return item[index] === player;
         })
       ) {
-        setwin("Winner")
+       win = "Winner"
         return `Winner  Player ${player} 🎉`;
+      
       }
     }
   };
-
   return (
     <div className="conainer max-w-2xl mx-auto p-20">
       <h1 className="text-center ">XO Game</h1>
+
       <h1 className="text-center">{checkWin()}</h1>
       <div className=" bg-indigo-200   grid grid-cols-3 border-solid gap-2  border-4 border-white " >  
-        {xo.map((x, index) => {
+        {item.map((prop, index) => {
           return (
             <button
               className="h-20   bg-indigo-400 "
               onClick={() => xoFunc(index)}
-            disabled={!(win === "" )|| !(xo[index] === "") }
-          // disabled={!(xo[index] === "")}
+            disabled={(win === "Winner" )|| !(item[index] === "") }
             >
-              <p className={`text-5xl ${x === "x"? 'text-blue-600': 'text-red-600'}`}  >{x}</p>
+              <p className={`text-5xl ${prop === "x"? 'text-blue-600': 'text-red-600'}`}  >{prop}</p>
             </button>
           );
         })}
       </div>
+     
+
     </div>
   );
 }
 
 export default XO;
+
+// function panel({title,children,isActive,onShow}){
+//   return (
+//     <section>
+//       <h1>{title}</h1>
+//       {isActive ? (
+//         <p></p>
+//       ) : (
+//         <button onClick={onShow}>
+//          start Game
+//         </button>
+//       )}
+//     </section>
+
+//   )
+
+// }
