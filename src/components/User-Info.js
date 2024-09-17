7a1fb17df2b4b5ca2fc5d562b7aab94e47275d53
Amoly93 <amalbajber93@gmail.com>
2024-09-17 15:22:43 +0300

    adding user list

--- src/components/User-Info.js
+++ src/components/User-Info.js
@@ -1,15 +1,34 @@
 import { useState } from "react";
 
 function UserInfo() {
-  const [myName, setMyName] = useState("");
+
+  const [name, setName] = useState("");
+  const [userList, setUserList] = useState([])
+const addFunction = (e)=>{
+    e.preventDefault(); 
+    userList.push(name);
+    setName("")
+}
 
   return (
-    <div className="">
-      <form className="space-x-5">
-        <label className="">Name:</label>
-        <input name="Name" onChange={(e) => setMyName(e.target.value)}></input>
+    <div>
+      <form className="space-x-5" onSubmit={addFunction}>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <button type="submit">Add</button>
       </form>
-      <p className="text-green-800 text-2xl text-center"> {myName}</p>
+   
+
+     {/* {userList.forEach(user => <p> {user}</p>)} */}
+     <p className="text-center ">Users</p>
+     {userList.map((user) => (
+          <p className="text-center text-green-800">{user}</p>
+        ))}
     </div>
   );
 }
