ff32a6b452a34b6037aa71ac63270e8d59047317
Amoly93 <amalbajber93@gmail.com>
2024-09-18 13:59:49 +0300

    edit functiom

--- src/components/User-Info.js
+++ src/components/User-Info.js
@@ -4,22 +4,33 @@ function UserInfo() {
   const [name, setName] = useState("");
   const [userList, setUserList] = useState([]);
   const [errors, setErrors] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+  const [editIndex, setEditIndex] = useState();
   const addFunction = (e) => {
     e.preventDefault();
     if (!name) {
       setErrors("This feild is required");
+    } else if (isEdit) {
+      const newArray = [...userList];
+      newArray.splice(editIndex, 1, name);
+      setUserList(newArray);
     } else {
       userList.push(name);
       setErrors("");
-      setName("");
     }
+
+    setName("");
   };
   const deleteFunc = (index) => {
     const newArray = [...userList];
     newArray.splice(index, 1);
     setUserList(newArray);
   };
-
+  const editFunc = (index) => {
+    setIsEdit(true);
+    setEditIndex(index);
+    setName(userList[index]);
+  };
   return (
     <div>
       <form
@@ -31,8 +42,8 @@ function UserInfo() {
           <div className="flex flex-col ">
             <input
               className={clsx(
-                " outline-none ring-2 ring-sky-600 h-7",
-                errors === "" ? "focus:ring-green-500" : " ring-red-500"
+                " outline-none ring-2  h-7",
+                errors === "" ? "focus:ring-green-500" : "ring-red-500"
               )}
               type="text"
               value={name}
@@ -46,17 +57,19 @@ function UserInfo() {
           className="rounded-md border-2 border-amber-500 h-8 w-8 m-1.5"
           type="submit"
         >
-          +
+          {isEdit ? "edit " : "+"}
         </button>
       </form>
 
       <p className="text-center underline font-semibold ">Users</p>
       <div className="divide-y divide-white">
-   
         {userList.map((user, index) => (
           <div className="flex justify-between flex-row px-5 py-2">
             <p className="text-center text-green-800">{user}</p>{" "}
-            <button onClick={() => deleteFunc(index)}>✖︎</button>
+            <div className="space-x-3.5">
+              <button onClick={() => editFunc(index)}>✎ </button>
+              <button onClick={() => deleteFunc(index)}>✖︎</button>
+            </div>
           </div>
         ))}
       </div>
