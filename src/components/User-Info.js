d3f1e2bc79de2869ea7593987c952195059987a4
Amoly93 <amalbajber93@gmail.com>
2024-09-18 10:01:05 +0300

    adding delete

--- src/components/User-Info.js
+++ src/components/User-Info.js
@@ -14,30 +14,48 @@ function UserInfo() {
       setName("");
     }
   };
+  const deleteFunc = (index) => {
+    const newArray = [...userList];
+    newArray.splice(index, 1);
+    setUserList(newArray);
+  };
 
   return (
     <div>
-      <form className="space-x-5" onSubmit={addFunction}>
-        <label>Name:</label>
-        <input
-          className={clsx(
-            "outline-none ring-2",
-            errors === "" ? "ring-sky-500" : " ring-red-500"
-          )}
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      <form
+        className="flex justify-between flex-row gap-20 p-5"
+        onSubmit={addFunction}
+      >
+        <div className="flex gap-2">
+          <label>Name:</label>
+          <div className="flex flex-col ">
+            <input
+              className={clsx(
+                " top-0 outline-none ring-2",
+                errors === "" ? "focus:ring-green-500" : " ring-red-500"
+              )}
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <span className="text-red-600"> {errors} </span>
+          </div>
+        </div>
 
-        <button type="submit">Add</button>
+        <button
+          className="rounded-md border-2 border-indigo-600 h-8 "
+          type="submit"
+        >
+          Add
+        </button>
       </form>
-      <span className="text-red-600"> {errors} </span>
 
       <p className="text-center ">Users</p>
-      {userList.map((user,index) => (
-        <div className="flex justify-center flex-row gap-10">   <p className="text-center text-green-800">{user}</p> <button >✖︎</button></div>
-      
-    
+      {userList.map((user, index) => (
+        <div className="flex justify-between flex-row ">
+          <p className="text-center text-green-800">{user}</p>{" "}
+          <button onClick={() => deleteFunc(index)}>✖︎</button>
+        </div>
       ))}
     </div>
   );
