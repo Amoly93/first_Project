72190610164accaa8a8464adad6bbc89c101ae59
yousifmedhat98 <y.medhat@bimventures.com>
2024-09-19 10:04:36 +0300

    Adding improvements

--- src/components/User-Info.js
+++ src/components/User-Info.js
@@ -1,80 +1,83 @@
 import { useState } from "react";
 import clsx from "clsx";
+
 function UserInfo() {
   const [name, setName] = useState("");
-  const [userList, setUserList] = useState([]);
+  const [usersList, setUsersList] = useState([]);
   const [errors, setErrors] = useState("");
   const [isEdit, setIsEdit] = useState(false);
-  const [editIndex, setEditIndex] = useState();
+
   const addFunction = (e) => {
     e.preventDefault();
     if (!name) {
       setErrors("This feild is required");
     } else if (isEdit) {
-      const newArray = [...userList];
-      newArray.splice(editIndex, 1, name);
-      setUserList(newArray);
+      const newArray = [...usersList];
+      const getUserIndex = usersList.findIndex((props) => props === name);
+      newArray.splice(getUserIndex, 1, name);
+      setUsersList(newArray);
       setIsEdit(false);
+      setErrors("");
     } else {
-      userList.push(name);
+      usersList.push(name);
       setErrors("");
     }
-
     setName("");
   };
-  const deleteFunc = (index) => {
-    const newArray = [...userList];
+
+  const onDelete = (index) => {
+    const newArray = [...usersList];
     newArray.splice(index, 1);
-    setUserList(newArray);
+    setUsersList(newArray);
   };
-  const editFunc = (index) => {
+
+  const onUpdate = (user) => {
     setIsEdit(true);
-    setEditIndex(index);
-    setName(userList[index]);
+    setName(user);
   };
+
   return (
-    <div>
+    <div className="space-y-8">
       <form
-        className="flex justify-between flex-row gap-20  p-5"
+        className="flex justify-between items-end gap-5"
         onSubmit={addFunction}
       >
-        <div className="">
-          <label className="">Name:</label>
-          <div className="flex flex-col ">
+        <div className="space-y-1">
+          <label>Name:</label>
+          <div className="flex flex-col gap-1">
             <input
               className={clsx(
-                " h-8",
-                errors && " outline-none ring-2 ring-red-500"
+                "h-10 px-1.5 rounded-md",
+                errors && "outline-none ring-2 ring-red-500"
               )}
-              type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
-            <span className="text-red-600"> {errors} </span>
+            {errors && <span className="text-red-600 text-xs">{errors}</span>}
           </div>
         </div>
-
         <button
-          className="bg-orange-500 px-6 text-white text-sm hover:bg-neutral-800 rounded-lg h-10 mt-5 "
+          className="bg-orange-500 px-6 text-white text-sm hover:bg-neutral-800 rounded-md h-10"
           type="submit"
         >
           {isEdit ? "Update" : "Add"}
         </button>
       </form>
-
-      <p className="font-semibold ">
-        {userList.length ? "Users" : "No users added yet"}
-      </p>
-      <div className="divide-y divide-white">
-        {userList.map((user, index) => (
-          <div className="flex justify-between flex-row px-5 py-2">
-            <p className="text-center text-green-800">{user}</p>{" "}
-            <div className="space-x-3.5">
-              <button onClick={() => editFunc(index)}>✎ </button>
-              <button onClick={() => deleteFunc(index)}>✖︎</button>
+      <div>
+        <p className="font-semibold text-center">
+          {usersList.length ? "Users" : "No users added yet"}
+        </p>
+        <div className="divide-y divide-white">
+          {usersList.map((user, index) => (
+            <div className="flex justify-between flex-row px-5 py-2">
+              <p className="text-center text-green-800">{user}</p>
+              <div className="space-x-3.5">
+                <button onClick={() => onUpdate(user)}>✎</button>
+                {!isEdit && <button onClick={() => onDelete(index)}>✖︎</button>}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
