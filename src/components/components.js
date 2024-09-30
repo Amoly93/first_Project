c9097e0ecb1c852297b160468c4a33fdefa76baa
Amoly93 <amalbajber93@gmail.com>
2024-09-30 11:18:15 +0300

    ui edit

--- src/components/components.js
+++ src/components/components.js
@@ -2,7 +2,7 @@ import XO from "./xo";
 import { useState } from "react";
 import UserInfo from "./user-info";
 import GetInTouch from "./get-in-touch-form";
-
+import AboutEnergy from "./about-energy";
 export const defaultItems = Array(9).fill("");
 
 function Components() {
@@ -13,11 +13,10 @@ function Components() {
     <>
       <UserInfo players={players} setPlayers={setPlayers} items={items} />
       <XO players={players} items={items} setItems={setItems} />
-<GetInTouch/>
+      <AboutEnergy />
+      <GetInTouch />
     </>
   );
 }
 
 export default Components;
-
-
