214f03cf21539f674aa0040b722f31201fcf2af3
Amoly93 <amalbajber93@gmail.com>
2024-09-30 14:07:35 +0300

    adding required

--- src/components/get-in-touch-form.js
+++ src/components/get-in-touch-form.js
@@ -70,7 +70,6 @@ function GetInTouch() {
                     className="bg-red-500 text-white px-4 py-2 rounded-md mb-4"
                     onClick={() => remove(index)}
                   >
-                    {" "}
                     {"✖︎"}
                   </button>
                 </div>
--- src/components/input-controller.js
+++ src/components/input-controller.js
@@ -1,36 +1,27 @@
+import React from "react";
 
-import React from "react"
+import { Controller } from "react-hook-form";
 
-import { Controller } from "react-hook-form"
-
-
-function InputController({control,placeholderName ,inputName ,required}) {
-
-
+function InputController({ control, placeholderName, inputName }) {
   return (
-
-      <Controller
-        control={control}
-        name={inputName}
-        rules={ required}
-        render={({ field: { onChange, onBlur, } ,fieldState:{error}}) => (
-          <div>
+    <Controller
+      control={control}
+      name={inputName}
+      rules={{ required: "This field is required" }}
+      render={({ field: { onChange, onBlur }, fieldState: { error } }) => (
+        <div>
           <input
-             className="px-2 h-10 w-full"
+            className="px-2 h-10 w-full"
             onChange={onChange}
             onBlur={onBlur}
             placeholder={placeholderName}
           />
-           {/* <span className="text-red-600 text-xs">
-              {error.message}
-            </span> */}
-
-            </div>
-        )}
-      
-      />
-
-
-  )
+          {error && (
+            <span className="text-red-600 text-xs">{error.message}</span>
+          )}
+        </div>
+      )}
+    />
+  );
 }
 export default InputController;
