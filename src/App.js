e2532960c546951d7ba2fa3873a4c1bc908ed3f7
Amoly93 <amalbajber93@gmail.com>
2024-10-02 16:34:19 +0300

    navLink

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate, NavLink } from "react-router-dom";
 import { AuthContext } from "./contexts/auth-context";
+import clsx from "clsx";
 
 const userInfo = {
   name: "Amal Bajaber",
@@ -17,6 +18,28 @@ function App() {
 
   return (
     <AuthContext.Provider value={userInfo}>
+      <div class="flex  items-center justify-center p-2 sm:items-stretch sm:justify-start">
+        <div class="hidden sm:ml-6 sm:block">
+          <div class="flex space-x-4">
+            <NavLink  to="home"   className={({ isActive }) =>
+                clsx(isActive ? "bg-orange-500 text-white" : "text-gray-300", "p-2")
+              }>
+            Home
+            </NavLink>
+            <NavLink  to="details"   className={({ isActive }) =>
+                clsx(isActive ? "bg-orange-500 text-white" : "text-gray-300", "p-2 ")
+              }>
+           Details
+            </NavLink>
+            <NavLink  to="details"   className={({ isActive }) =>
+                clsx(isActive ? "bg-orange-500 text-white" : "text-gray-300", "p-2")
+              }>
+           Login
+            </NavLink>
+            
+          </div>
+        </div>
+      </div>
       <Outlet />
     </AuthContext.Provider>
   );
