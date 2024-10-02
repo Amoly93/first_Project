b8eebcbd6d7fa5e7abdc234830e43ca7f54c2c09
Amoly93 <amalbajber93@gmail.com>
2024-10-02 14:53:49 +0300

    fix create hook

--- src/components/hook.js
+++ src/components/hook.js
@@ -1,10 +1,8 @@
 import { useContext } from "react";
-import { SubjectContext } from "../context"; // Adjust the path as necessary
+import { SubjectContext } from "../context"; 
 
 export const useSubject = () => {
     const context = useContext(SubjectContext);
-    if (!context) {
-        throw new Error("useSubject must be used within a SubjectProvider");
-    }
+
     return context;
 };
