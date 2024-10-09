40fc1fe5014e3b4454e78ba76d91cd0e62799568
yousifmedhat98 <y.medhat@bimventures.com>
2024-10-09 10:05:01 +0300

    Update @tanstack/react-query to version 5.59.3

---
+++ src/pages/users-page.js
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const Users = () => {
+  const fetchData = async () => {
+    const response = await axios.get("https://jsonplaceholder.org/users");
+    return response.data;
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["dataKey"],
+    queryFn: fetchData,
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching data: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      {data.map((user) => (
+        <p key={user.id} className="p-2">
+          {user.firstname} {user.lastname}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
